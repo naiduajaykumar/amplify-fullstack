a5e30420bf83e377d22266feb0124f808614c84e
naiduajaykumar <mailtonaiduajaykumar@gmail.com>
2024-03-27 14:15:37 -0400

    add signout1

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App({signOut}) {
     <View className="App">
       <Card>
         <Image src={logo} className="App-logo" alt="logo"/>
-        <Heading level={1}>We have now Authentation</Heading>
+        <Heading level={1}>We have now Authentation in our App</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
