2ab2b7625f5c79c56bb6140bb10e98bc56190468
naiduajaykumar <mailtonaiduajaykumar@gmail.com>
2024-03-27 13:21:51 -0400

    add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+  withAuthenticator
+} from "@aws-amplify/ui-react";
 
-function App() {
+
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Fist App Deployed in AWS CLoud
-        </a>
-      </header>
-    </div>
-  );
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo"/>
+        <Heading level={1}>We have now Authentation</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  )
 }
-
-export default App;
